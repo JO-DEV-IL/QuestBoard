50ceb79a06e09ca1c5d7aa460a540bd0a7629dc7
JO-DEV-IL <108283760+JO-DEV-IL@users.noreply.github.com>
2023-02-24 19:39:58 -0500

    updates
    stat toggle, stat card css, working on a toggleable friend's list

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleStats() {
+    const stats = document.getElementById("char-stats-partial");
+    if (stats.style.display === "none") {
+        stats.style.display = "block";
+    } else {
+        stats.style.display = "none";
+    }
+}
