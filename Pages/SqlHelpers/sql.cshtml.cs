d4d38e0c32a5827aaa78a0e48cc0c3bbcbadcc1f
JO-DEV-IL <108283760+JO-DEV-IL@users.noreply.github.com>
2023-05-02 21:21:54 -0400

    character panel updates

--- Pages/SqlHelpers/sql.cshtml.cs
+++ Pages/SqlHelpers/sql.cshtml.cs
@@ -23,15 +23,34 @@ namespace QuestBoard.Pages.SqlHelpers
         public class UserEquipment
         {
             public String head;
+            public String head_img;
+
             public String shoulders;
+            public String shoulders_img;
+
             public String chest;
+            public String chest_img;
+
             public String hands;
+            public String hands_img;
+
             public String legs;
+            public String legs_img;
+
             public String feet;
+            public String feet_img;
+
             public String mainhand;
+            public String mainhand_img;
+
             public String offhand;
+            public String offhand_img;
+
             public String accessory1;
+            public String accessory1_img;
+
             public String accessory2;
+            public String accessory2_img;
         }
 
         public class ShopItems
@@ -115,15 +134,34 @@ namespace QuestBoard.Pages.SqlHelpers
                         UserEquipment userEquipment = new UserEquipment();
 
                         userEquipment.head = reader.GetString(1);
-                        userEquipment.shoulders = reader.GetString(2);
-                        userEquipment.chest = reader.GetString(3);
-                        userEquipment.hands = reader.GetString(4);
-                        userEquipment.legs = reader.GetString(5);
-                        userEquipment.feet = reader.GetString(6);
-                        userEquipment.mainhand = reader.GetString(7);
-                        userEquipment.offhand = reader.GetString(8);
-                        userEquipment.accessory1 = reader.GetString(9);
-                        userEquipment.accessory2 = reader.GetString(10);
+                        userEquipment.head_img = reader.GetString(2);
+
+                        userEquipment.shoulders = reader.GetString(3);
+                        userEquipment.shoulders_img = reader.GetString(4);
+
+                        userEquipment.chest = reader.GetString(5);
+                        userEquipment.chest_img = reader.GetString(6);
+
+                        userEquipment.hands = reader.GetString(7);
+                        userEquipment.hands_img = reader.GetString(8);
+
+                        userEquipment.legs = reader.GetString(9);
+                        userEquipment.legs_img = reader.GetString(10);
+
+                        userEquipment.feet = reader.GetString(11);
+                        userEquipment.feet_img = reader.GetString(12);
+
+                        userEquipment.mainhand = reader.GetString(13);
+                        userEquipment.mainhand_img = reader.GetString(14);
+
+                        userEquipment.offhand = reader.GetString(15);
+                        userEquipment.offhand_img = reader.GetString(16);
+
+                        userEquipment.accessory1 = reader.GetString(17);
+                        userEquipment.accessory1_img = reader.GetString(18);
+
+                        userEquipment.accessory2 = reader.GetString(19);
+                        userEquipment.accessory2_img = reader.GetString(20);
 
                         listEquipment.Add(userEquipment);
                     }
